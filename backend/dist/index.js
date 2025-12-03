b6e6b73f56f53126bb2e635a8d68cc6f8469f8c9
Dev kumar <devkmandal0@gmail.com>
2025-12-04 03:50:01 +0530

    feat: improve WebSocket room management and connection handling

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -28,28 +28,30 @@ client.$connect().then(() => {
         });
     });
     wss.on('connection', (socket, req) => {
-        console.log("socket connected");
+        socket.send(200);
         const parsedUrl = url.parse(req.url || "", true);
         const query = parsedUrl.query;
-        switch (query.type) {
-            case "create":
+        if (query.type === "create") {
+            if (rooms.length == 0) {
                 rooms.push({ roomId: Number(query.roomId), users: [socket] });
-                console.log(rooms);
-                break;
-            case "join":
-                const filtered = rooms.filter((e) => e.roomId === Number(query.roomId));
-                filtered[0]?.users.push(socket);
-                console.log(filtered);
-                socket.on("message", (data) => {
-                    filtered[0]?.users.forEach((user) => {
-                        if (user.readyState === 1) {
-                            if (socket != user)
-                                user.send(data.toString());
-                        }
-                    });
+            }
+            else {
+                const isAvailable = rooms.find(data => data.roomId === Number(query.roomId));
+                !isAvailable && rooms.push({ roomId: Number(query.roomId), users: [socket] });
+            }
+        }
+        else if (query.type === "join") {
+            const filtered = rooms.filter((e) => e.roomId === Number(query.roomId));
+            filtered[0]?.users?.length == 0 && filtered[0].users.push(socket);
+            !filtered[0]?.users.find(data => data == socket) && filtered[0]?.users.push(socket);
+            socket.on("message", (data) => {
+                filtered[0]?.users.forEach((user) => {
+                    if (user.readyState === 1) {
+                        if (socket != user)
+                            user.send(data.toString());
+                    }
                 });
-            default:
-                break;
+            });
         }
     });
     server.listen(port, () => console.log("server is running on " + port));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import ErrorMiddleware from './middleware/error.middleware.js';
 import bodyParser from 'body-parser';
 import express from 'express';
 import cookieParser from 'cookie-parser'
-import {WebSocketServer,WebSocket } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 import http from 'http'
 import url, { type UrlWithParsedQuery } from 'url'
 
@@ -28,11 +28,7 @@ interface Rooms {
     roomId: number;
     users: WebSocket[];
 }
-interface Query{
-    roomId:number;
-    type:string;
-    user:string;
-}
+
 
 const rooms: Rooms[] = [];
 
@@ -47,32 +43,38 @@ client.$connect().then(() => {
     })
 
     wss.on('connection', (socket, req) => {
-        console.log("socket connected")
+
+        socket.send(200)
         const parsedUrl = url.parse(req.url || "", true);
         const query = parsedUrl.query;
 
 
-        switch (query.type) {
-            case "create":
+        if (query.type === "create") {
+         if(rooms.length==0){
                 rooms.push({ roomId: Number(query.roomId), users: [socket] });
-                console.log(rooms)
-                break;
-            case "join":
-                const filtered:Rooms[]= rooms.filter((e:Rooms) => e.roomId === Number(query.roomId));
-                filtered[0]?.users.push(socket)
-                console.log(filtered)
-                
-                socket.on("message",(data)=>{
-                    filtered[0]?.users.forEach((user)=>{
-                        if(user.readyState===1){
-                            if(socket!=user)user.send(data.toString());
-                            
-                        }
-                    })
+         }else{
+            const isAvailable = rooms.find(data=>data.roomId===Number(query.roomId))
+            !isAvailable && rooms.push({ roomId: Number(query.roomId), users: [socket] });
+         }
+
+        } else if (query.type === "join") {
+            const filtered: Rooms[] = rooms.filter((e: Rooms) => e.roomId === Number(query.roomId));
+            
+            filtered[0]?.users?.length==0 && filtered[0].users.push(socket)
+            !filtered[0]?.users.find(data=>data==socket)&& filtered[0]?.users.push(socket);
+            
+
+            socket.on("message", (data) => {
+                filtered[0]?.users.forEach((user) => {
+                    if (user.readyState === 1) {
+                        if (socket != user) user.send(data.toString());
+                    }
                 })
-            default:
-                break;
+            })
         }
+
+
+
     })
 
     server.listen(port, () => console.log("server is running on " + port));
