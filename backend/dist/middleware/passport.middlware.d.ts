21394ec835a0a536f320f6f1d927da03ff15f668
Dev kumar <devkmandal0@gmail.com>
2025-12-31 23:26:45 +0530

    feat: add passport middleware files and update tsconfig to include new middleware

---
+++ backend/dist/middleware/passport.middlware.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=passport.middlware.d.ts.map
---
+++ backend/dist/middleware/passport.middlware.js
@@ -0,0 +1,4 @@
+import { verify } from 'jsonwebtoken';
+import passport from 'passport';
+import { Strategy } from 'passport-google-oauth20';
+//# sourceMappingURL=passport.middlware.js.map
